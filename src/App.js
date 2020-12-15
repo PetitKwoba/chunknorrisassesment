8d2c0ac1a596a726dbbf298f580e5a1a931b919c
Petit Emmanuel <kwobapetit@gmail.com>
2020-12-15 21:02:57 +0300

    reached categorization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import {
+    Button,
+    Card,
+    CardContent,
+    CardActions,
+    Chip,
+    Container,
+    Typography,
+} from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    card: {
+        marginBottom: 20,
+    },
+    CardContent: {
+        paddingBottom: '5'
+    },
+    CardActions: {
+        padding: '15px'
+    }
+})
+const likeJoke = (id) => {
+    console.log("liking joke, " + id);
+}
+
+const unlikeJoke = (id) => {
+    console.log("uliking joke, " + id);
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [jokes, setJokes] = useState([])
+
+    const classes = useStyles();
+    useEffect(() => {
+        fetch('https://api.icndb.com/jokes')
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                setJokes(res.value)
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    return ( <
+            div className = "App" >
+            <
+            CssBaseline / >
+            <
+            Container >
+            <
+            Typography variant = "h1"
+            align = "center" >
+            Chuck Norris jokes <
+            /Typography>  {
+            jokes.map(joke => ( <
+                Card key = { joke.id }
+                className = { classes.card } > <
+                CardContent className = { classes.CardContent } >
+
+                {
+                    joke.categories.length > 0 ? (
+                        joke.categories.map(cat => ( <
+                            Chip label = { cat }
+                            key = { cat }
+                            variant = "outlined"
+                            style = {
+                                { marginTop: 10, marginBottom: 10 }
+                            }
+                            / >
+                        ))
+                    ) : < Chip label = "Not Categorized"
+                    variant = "outlined"
+                    style = {
+                        { marginTop: 10, marginBottom: 10 }
+                    }
+                    / >
+                }
+
+                <
+                Typography > { joke.joke } < /Typography> < /
+                CardContent >
+                <
+                CardActions className = { classes.CardActions } >
+
+                <
+                Button variant = "contained"
+                color = "primary"
+                onClick = {
+                    () => likeJoke(joke.id)
+                } >
+                like <
+                /Button> 
+
+                <
+                Button variant = "contained"
+                color = "default"
+                onClick = {
+                    () => unlikeJoke(joke.id)
+                } >
+                unlike <
+                /Button> < /
+                CardActions > <
+                /Card>
+            ))
+        } <
+        /
+    Container > <
+        /
+    div >
+);
 }
 
 export default App;
